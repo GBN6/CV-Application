6bf6fb52a98977c0ad4734404cec774659032f4f
GBN6 <czesiox8@gmail.com>
2022-09-20 15:20:51 +0200

    Create initial components and structure

---
+++ src/components/Experience.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <form>
+      <input type='text' name='' placeholder='Company' />
+      <input type='text' name='' placeholder='City' />
+      <input type='text' name='' placeholder='Position' />
+      <input type='text' name='' placeholder='Details' />
+      <input type='text' name='' placeholder='From' />
+      <input type='text' name='' placeholder='To' />
+    </form>
+  );
+}
+
+export default Experience;
